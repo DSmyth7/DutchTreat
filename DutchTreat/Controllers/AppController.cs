194801fba4ccd670034792e4c5c845444b855416
DSmyth7 <d-jsmyth@hotmail.co.uk>
2019-01-29 14:05:12 +0000

    Completed Using ASP.NET MVC section

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        } 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("donal.smyth@neueda.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+    }
+}
